76097b2934e1da4b0c60cf0ed072e8757a162f32
Rafi <mcintoshhd@Mcintoshs-Air.gateway>
2024-07-01 19:48:16 +1000

    setting up chat UI with input, button and layout for chat

--- app/(dashboard)/chat/page.tsx
+++ app/(dashboard)/chat/page.tsx
@@ -1,9 +1,14 @@
+import Chat from "@/components/Chat";
 import React from "react";
 
 interface ChatPageProps {}
 
 const ChatPage = ({}: ChatPageProps) => {
-  return <div>ChatPage</div>;
+  return (
+    <>
+      <Chat />
+    </>
+  );
 };
 
 export default ChatPage;
---
+++ components/Chat.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { FormEvent, useState } from "react";
+
+interface ChatProps {}
+
+const Chat = ({}: ChatProps) => {
+  const [text, setText] = useState("");
+  const [message, setMessage] = useState([]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(text);
+  };
+  return (
+    <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
+      <div>
+        <h2 className="text-5xl">messages</h2>
+      </div>
+      <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
+        <div className="join w-full">
+          <input
+            type="text"
+            placeholder="message GPTGenius"
+            className="input input-bordered join-item w-full"
+            value={text}
+            required
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button className="btn btn-primary join-item" type="submit">
+            Ask Question
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
