0bf33c1748ea8302b0804e291942eedc1270a4fc
Rafi <mcintoshhd@Mcintoshs-Air.gateway>
2024-07-04 20:29:53 +1000

    integrate data tour to TourInfo component

--- components/TourInfo.tsx
+++ components/TourInfo.tsx
@@ -6,7 +6,20 @@ interface TourInfoProps {
 }
 
 const TourInfo = ({ tour }: TourInfoProps) => {
-  return <div>TourInfo</div>;
+  const { title, description, stops } = tour;
+  return (
+    <div className="max-w-2xl">
+      <h1 className="text-4xl font-semibold mb-4">{title}</h1>
+      <p className="leading-loose mb-6">{description}</p>
+      <ul>
+        {stops.map((stop) => (
+          <li key={stop} className="mb-4 bg-base-100 p-4 rounded-xl">
+            <p>{stop}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TourInfo;
